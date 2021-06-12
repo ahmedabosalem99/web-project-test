23568d42460d13f4ec8b04f85d915e5769fc0ab0
ahmedabosalem99 <66332650+ahmedabosalem99@users.noreply.github.com>
2021-06-12 23:28:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,4 +7,35 @@ function myFunction() {
       x.style.display = "block";
     }
   }
-  
+  function myFunction1() {
+    var x = document.getElementById("productivity");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
+  function myFunction2() {
+    var x = document.getElementById("habits");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
+  function myFunction3() {
+    var x = document.getElementById("skills");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
+  function myFunction4() {
+    var x = document.getElementById("arts");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
